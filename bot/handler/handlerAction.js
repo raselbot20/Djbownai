c4e7234540c9723d5bca7e9a6bbdd74bc90b92cc
raselbot20 <bot571847@gmail.com>
2025-12-23 22:11:19 +0600

    Update handlerAction.js

--- bot/handler/handlerAction.js
+++ bot/handler/handlerAction.js
@@ -82,7 +82,7 @@ api.removeUserFromGroup(event.senderID, event.threadID, (err) => {
     message.send(":)")
   }
   }
-        if(event.reaction ==  "〰️"){
+        if(event.reaction ==  "®️"){
   if(event.senderID == api.getCurrentUserID()){if(event.userID == "61580043347594","100024220812646"){
     message.unsend(event.messageID)
 }else{

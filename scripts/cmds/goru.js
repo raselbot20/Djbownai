7702aa3f3eb3ac7105ef87d89f11a781e0020647
raselbot20 <bot571847@gmail.com>
2026-01-20 21:00:01 +0600

    Update goru.js

--- scripts/cmds/goru.js
+++ scripts/cmds/goru.js
@@ -1,4 +1,4 @@
-* cmd install goru.js const fs = require("fs-extra");
+const fs = require("fs-extra");
 const path = require("path");
 const axios = require("axios");
 const { createCanvas, loadImage } = require("canvas");

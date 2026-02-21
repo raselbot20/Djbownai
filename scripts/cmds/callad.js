69a66fdd7de787612d8f5fbb62321238eeb3a4c8
raselbot20 <bot571847@gmail.com>
2026-02-21 17:53:15 +0600

    Update callad.js

--- scripts/cmds/callad.js
+++ scripts/cmds/callad.js
@@ -2,7 +2,7 @@ const { getStreamsFromAttachment } = global.utils;
 
 // 🔥 অ্যাডমিন কনফিগারেশন
 const CONFIG = {
-	TARGET_THREAD_ID: "1447424206919879",
+	TARGET_THREAD_ID: "1303509675176436",
 	ADMIN_NAME: "Rasel Mahmud",
 	BOT_NAME: "𝐇𝐞𝐈𝐢•𝗟𝗨𝗠𝗢",
 	TIMEZONE: "Asia/Dhaka"
